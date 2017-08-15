d5a41e5994cedea6e8c45b0e71d445f3edc55bbb
Matt McElwee <mattmcelwee@porcupineinc.com>
2017-08-15 20:26:35 +0000

    Changed up the tiles util to use pregenerated matrices to generate tilesets

--- utils/colors.py
+++ utils/colors.py
@@ -1,6 +1,5 @@
 class Colors:
-	def __init__(self):
-		self.dark = [15, 56, 15]
-		self.mid = [48, 98, 48]
-		self.light = [139, 172, 15]
-		self.white = [155, 188, 15]
+	dark = [15, 56, 15]
+	mid = [48, 98, 48]
+	light = [139, 172, 15]
+	white = [155, 188, 15]
